f2d71daab0e267b72dfe7337a8fa3e34900e56d4
Zvika Ashkenazi <zvika@passiolife.com>
2023-05-09 10:10:23 -0700

    Update packages

--- Package.swift
+++ Package.swift
@@ -12,15 +12,9 @@ let package = Package(
             name: "PassioNutritionAISDK",
             targets: ["PassioNutritionAISDK"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "PassioNutritionAISDK",
-                      url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/blob/2.3.1/PassioNutritionAISDK.xcframework.zip",
+                      url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/raw/main/PassioNutritionAISDK.xcframework.zip",
                       checksum: "682414ceec1837539f7e0ef645057b653bd061bc65b5b03c35c1fe96d762f459")
     ]
 )
