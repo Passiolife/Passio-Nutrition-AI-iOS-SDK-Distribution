05a0a0d7fe9438249b5250739a016ebb92f62f85
Zvika Ashkenazi <zvika@passiolife.com>
2023-05-09 08:01:08 -0700

    Added Packages

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PassioNutritionAISDK",
+    products: [
+        .library(
+            name: "PassioNutritionAISDK",
+            targets: ["PassioNutritionAISDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "PassioNutritionAISDK",
+                      url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/blob/main/PassioNutritionAISDK.xcframework.zip",
+                      checksum: "682414ceec1837539f7e0ef645057b653bd061bc65b5b03c35c1fe96d762f459")
+    ]
+)
