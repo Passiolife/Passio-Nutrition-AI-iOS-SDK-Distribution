f4cb55976062fe0f63e1faa2076f52504b334239
Zvika Ashkenazi <zvika@passiolife.com>
2023-05-09 09:11:31 -0700

    update package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "PassioNutritionAISDK",
+    platforms: [.iOS(.v13)
+    ],
     products: [
         .library(
             name: "PassioNutritionAISDK",
