cc470ef7b0cf9a04e3081069e639f5f7081c127c
Zvika Ashkenazi <zvika@passiolife.com>
2023-08-28 07:22:43 -0700

    Fix tags 2.3.7

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "PassioNutritionAISDK",
-                      url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/raw/2.3.8/PassioNutritionAISDK.xcframework.zip",
-                      checksum: "c6613abef021eea7b2cd5193afb653bcbbeffc849f5c3887036c2cc0614b65fa")
+                      url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/raw/2.3.7/PassioNutritionAISDK.xcframework.zip",
+                      checksum: "b5ddaddb4662490de56b2bd3e4450813ee91e082ae16963bd0e0eb6b597c2da0")
     ]
 )
