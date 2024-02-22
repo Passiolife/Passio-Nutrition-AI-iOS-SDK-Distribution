3bf6ea014c0642bbe3b8d5a05f683d4913de7592
Harsh Parikh <harsh.parikh@mindinventory.com>
2024-02-22 17:32:29 +0530

    Updated XCFramework

--- Package.swift
+++ Package.swift
@@ -15,6 +15,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "PassioNutritionAISDK",
                       url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/raw/3.0.1/PassioNutritionAISDK.xcframework.zip",
-                      checksum: "6a88c1a4a2ca7d1fcbde8abe1270c9d1860a58a52c3b5a1e0230266ac78d63b1")
+                      checksum: "602908bf29fe6d788dd12ad7e10affa004a2397bec6691b5f857ba3b733eb733")
     ]
 )
