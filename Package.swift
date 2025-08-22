cdf5899d6549753c2d056dd38f293923d913852e
Davido Hyer <davidohyer@churchofjesuschrist.org>
2025-08-22 14:25:23 -0600

    Fixed checksum

--- Package.swift
+++ Package.swift
@@ -15,6 +15,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "PassioNutritionAISDK",
                       url: "https://github.com/Passiolife/Passio-Nutrition-AI-iOS-SDK-Distribution/raw/3.2.9/PassioNutritionAISDK.xcframework.zip",
-                      checksum: "bef6ad2b7e1a1e3163ceaf94fe25109a14363e2b530e79d1659db5c98c8db8f4")
+                      checksum: "c1b4cd6c6bdd07cb8ce0739f05b390e83c6ff0a9f96e8eae05d3eb50617ada54")
     ]
 )
